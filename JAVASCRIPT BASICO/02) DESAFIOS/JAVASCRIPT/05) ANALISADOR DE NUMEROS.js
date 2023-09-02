5b5117e58fc55073f894e5f071bd2dac43f909da
CODIGOS™ <107000099+VILHALVA@users.noreply.github.com>
2023-09-02 10:44:15 -0500

    Update 05) ANALISADOR DE NUMEROS.js

--- JAVASCRIPT BASICO/02) DESAFIOS/JAVASCRIPT/05) ANALISADOR DE NUMEROS.js
+++ JAVASCRIPT BASICO/02) DESAFIOS/JAVASCRIPT/05) ANALISADOR DE NUMEROS.js
@@ -1,66 +1,61 @@
-let num = document.querySelector('input#fnum')
-let lista = document.querySelector('select#flista')
-let res = document.querySelector('div#res')
-let valores = []
+document.addEventListener('DOMContentLoaded', function() {
+    let num = document.querySelector('input#fnum');
+    let lista = document.querySelector('select#flista');
+    let res = document.querySelector('div#res');
+    let valores = [];
 
-function isNumero(n) {
-    if (Number(n) >= 1 && Number(n) <= 100) {
-        return true
+    function isNumero(n) {
+        return Number(n) >= 1 && Number(n) <= 100;
     }
-    else {
-        return false
-    }
-}
 
-function inLista(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
-        return true
+    function inLista(n, l) {
+        return l.indexOf(Number(n)) !== -1;
     }
-    else {
-        return false
-    }
-}
 
-function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value, valores)) {
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `O VALOR ${num.valores} FOI ADICIONADO!`
-        lista.appendChild(item)
-        res.innerHTML = ''
-    }
-    else {
-        window.alert("ERRO: VALOR INVÁLIDO OU REPETIDO!")
-    }
-    num.value = ''
-    num.focus()
-}
-
-function finalizar() {
-    if (valores.length == 0) {
-        window.alert("ADICIONE VALORES!")
-    }
-    else {
-        let tot = valores.length
-        let maior = valores[0]
-        let menor = valores[0]
-        let soma = ''
-        let media = ''
-        for (let pos in valores) {
-            soma += valores[pos]
-            if (valores[pos] > maior) {
-                maior = valores[pos]
-            }
-            if (valores[pos] < menor) {
-                menor = valores[pos]
-            }
+    function adicionar() {
+        if (isNumero(num.value) && !inLista(num.value, valores)) {
+            valores.push(Number(num.value));
+            let item = document.createElement('option');
+            item.text = `O VALOR ${num.value} FOI ADICIONADO!`;
+            lista.appendChild(item);
+            res.innerHTML = '';
+        } 
+        else {
+            window.alert("ERRO: VALOR INVÁLIDO OU REPETIDO!");
         }
-        media = soma / tot
-        res.innerHTML = ''
-        res.innerHTML += `<p>AO TODO TEMOS ${tot} VALORES!</p>`
-        res.innerHTML += `<p>O MAIOR VALOR É ${maior}.</p>`
-        res.innerHTML += `<p>O MENOR VALOR É ${menor}.</p>`
-        res.innerHTML += `<p>A SOMA DOS VALORES É ${soma}.</p>`
-        res.innerHTML += `<p>A MEDIA DOS VALORES É ${media}.</p>`
+        num.value = '';
+        num.focus();
     }
-}
+
+    function finalizar() {
+        if (valores.length === 0) {
+            window.alert("ADICIONE VALORES!");
+        } 
+        else {
+            let tot = valores.length;
+            let maior = valores[0];
+            let menor = valores[0];
+            let soma = 0;
+            let media = 0;
+            for (let pos in valores) {
+                soma += valores[pos];
+                if (valores[pos] > maior) {
+                    maior = valores[pos];
+                }
+                if (valores[pos] < menor) {
+                    menor = valores[pos];
+                }
+            }
+            media = soma / tot;
+            res.innerHTML = '';
+            res.innerHTML += `<p>AO TODO TEMOS ${tot} VALORES!</p>`;
+            res.innerHTML += `<p>O MAIOR VALOR É ${maior}.</p>`;
+            res.innerHTML += `<p>O MENOR VALOR É ${menor}.</p>`;
+            res.innerHTML += `<p>A SOMA DOS VALORES É ${soma}.</p>`;
+            res.innerHTML += `<p>A MÉDIA DOS VALORES É ${media.toFixed(2)}.</p>`;
+        }
+    }
+
+    document.querySelector('#adicionar').addEventListener('click', adicionar);
+    document.querySelector('#finalizar').addEventListener('click', finalizar);
+});
